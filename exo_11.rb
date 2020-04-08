dbf4de78ffed9404322b4e6d8b53342985dd2aa2
Eternya <53874831+Eternya@users.noreply.github.com>
2020-04-08 04:15:28 +0200

    Update exo_11.rb

--- exo_11.rb
+++ exo_11.rb
@@ -1,11 +1,9 @@
 puts "Combien de fois tu veux afficher la phrase ?"
 print "> "
-number_of_elements = gets.chomp
+number_of_elements = gets.chomp.to_i
 puts number_of_elements
 
-number_of_elements = "Salut, ça farte ?"
-for number_of_elements in (1..10)
+number_of_elements.times do 
     puts "Salut, ça farte ?"
   end
-
  
